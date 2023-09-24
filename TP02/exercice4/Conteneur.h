ac508fbee7267ed21e127431a008816924373a3a
Kersual Arnaud <arnaud.kersuall@gmail.com>
2023-09-24 23:25:32 +0200

    Exercice 4 finished

--- TP02/exercice4/Conteneur.h
+++ TP02/exercice4/Conteneur.h
@@ -5,15 +5,115 @@
 #include <iostream>
 #include <stdexcept>
 
+using namespace std;
+
 template<class T>
 class Conteneur {
 
-// A COMPLETER : Spécifiez ici le template Conteneur<T>
+// Spécifiez ici le template Conteneur<T>
+public:
+    Conteneur();
+    // On doit passer obligatoirement pas un String mais bien un Objet de même type Conteneur.
+    // Const. par recopie
+    Conteneur(const Conteneur<T> & unConteneur);
 
+    // opérateur par affectation
+    Conteneur<T> & operator=(const Conteneur<T> & unConteneur);
+
+    inline ~Conteneur(){
+        this->clear();
+    }
+
+     void ajouter(T * element);
+     void afficher(ostream & sortie = cout) const;
+     // return reference to object
+     T & choisir(istream & entree = cin);
+private:
+    // Collection de pointeur sur des éléments.
+    //indicés (à partir de 0) extensible (réduire ou rallonger).
+    vector<T*> m_ptrs;
+    // Méthode qui sera appelée dans le constructeur avec argument.
+    void clone(const Conteneur<T> & unConteneur);
+    // Méthode pour vider le vecteur
+    void clear();
 };
 
-// A COMPLETER : Implémentez ici les méthodes du template Conteneur<T> (pas de fichier .cpp pour un template)
+//  Implémentez ici les méthodes du template Conteneur<T> (pas de fichier .cpp pour un template)
 
+template<class T>
+// ': m_ptrs()' vector default constructor. initializing the vector as empty, i.e., it starts with no elements.
+Conteneur<T>::Conteneur() : m_ptrs() {}
+
+// Appel la méthode Clone définie lors d'un const. par recopie.
+template <class T>
+Conteneur<T>::Conteneur(const Conteneur<T> & unConteneur) {
+    this->clone(unConteneur);
+}
+
+// T = classe distincte & Conteneur<T> = même class que this
+// Donc Conteneur<T> = permet l'accès à ses membres privés.
+// T = ne donne pas un accès directe aux membres privés de conteneurs.
+/*
+ *  En résumé, la visibilité des membres privés dépend de la portée et de la structure des classes en jeu.
+ *  Dans le premier cas, vous travaillez à l'intérieur de la classe Conteneur<T>,
+ *  ce qui permet l'accès à ses membres privés. Dans le second cas,
+ *  vous travaillez avec une classe distincte (T),
+ *  ce qui ne donne pas un accès direct aux membres privés de Conteneur<T>.
+ */
+template <class T>
+//  So, the unConteneur object itself remains unchanged, but its elements are cloned and added to the m_contenu vector of the current object.
+//  This allows you to create a copy of the contents of unConteneur without modifying unConteneur itself.
+void Conteneur<T>::clone(const Conteneur<T> & unConteneur){
+    for (T * elements : unConteneur.m_ptrs) {
+        // Allocation dynamique donc 'new'
+        this->m_ptrs.push_back(new T(*elements)); // les données pointées par l'élément sont copiées dans un nouvel objet T.
+    }
+}
+
+template <class T>
+Conteneur<T> & Conteneur<T>::operator=(const Conteneur<T> & unConteneur){
+    this->clear();
+    this->clone(unConteneur);
+    return *this;
+}
+
+template <class T>
+void Conteneur<T>::clear(){
+    // Delete all the elements in the vector.
+    for (T * elements : this->m_ptrs) {
+        delete(elements);
+    }
+}
+
+template <class T>
+void Conteneur<T>::ajouter(T * element){
+    this->m_ptrs.push_back(element); // Ajoute un élément de valeur e à la fin du vecteur
+}
+
+template <class T>
+void Conteneur<T>::afficher(ostream & sortie) const{
+    unsigned int i = 0;
+    sortie << "Voici ce qu'il y a dans le conteneur :" << endl;
+    for (T * elements : this->m_ptrs) {
+        sortie << i++ << " - " << *elements << endl;
+    }
+}
+
+template <class T>
+T & Conteneur<T>::choisir(std::istream & entree) {
+    unsigned int index;
+    cout << " Choissisez parmi un contenu entre 1 a " << this->m_ptrs.size() << endl;
+    entree >> index;
+    // Permet une fois validation de la saisit(integer) de nettoyer le caractère ('\n') = nouvelle ligne ou d'autres caractères restant dans le buffer(tampon).
+    entree.ignore(256, '\n');
+    if (index < 1 || index > this->m_ptrs.size()){
+        // L'exception 'range_error' est pertinente pour cette situation.
+        throw range_error("Element introuvable");
+    }
+    else{
+        return *(this->m_ptrs[index-1]); // or this->m_prts.at(index - 1);
+    }
+}
 
 #endif /* CONTENEUR_H */
 
--- TP02/exercice4/exercice4.cpp
+++ TP02/exercice4/exercice4.cpp
@@ -7,6 +7,40 @@ using namespace std;
 int main(int argc, char** argv) {
 
     // A COMPLETER : tester le template Conteneur<T>
-    
-    return 0;
+    Conteneur<string> conteneurStr;
+
+    // On instancie dynamiquement les éléments du conteneur avec (new)
+    conteneurStr.ajouter(new string("Bonjour"));
+    conteneurStr.ajouter(new string("Salut"));
+    conteneurStr.ajouter(new string("Yo"));
+    conteneurStr.ajouter(new string("Enchanter"));
+
+    // Test des méthodes afficher et choisir
+    bool continuer = true;
+    do {
+
+        cout << endl << "Contenu : " << endl;
+        conteneurStr.afficher();
+        cout << endl << "Numero choisi (0 pour finir) : ";
+        try {
+            string & element = conteneurStr.choisir();
+            cout << endl << "Vous avez choisi : " << element << endl;
+        } catch (range_error & exception) {
+            // si choisir a levé une exception
+            cout << "C'est fini..." << endl;
+            continuer = false;
+        }
+
+    } while (continuer); // boucle infinie...
+
+    // Test COPLIEN
+    Conteneur<string>* autreConteneur = new Conteneur<string>(conteneurStr); // const. par recopie
+    Conteneur<string> troisiemeConteneur = *autreConteneur; // opérateur affectation
+    delete autreConteneur; // On supprime lsdddde conteneur alloué dynamiquement
+    cout << "Contenu du conteneur obtenu par affectation d'un conteneur depuis disparu : " << endl;
+    troisiemeConteneur.afficher();
+
+    // conteneur et troisiemeConteneur, alloués dynamiquement, vont être libérés
+    // et leurs destructeurs appelés juste avant
+    return EXIT_SUCCESS;
 }
